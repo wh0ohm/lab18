c426c1868ca5fac6f825e210da04625a3843613f
wh0ohm <wararat.boonchieng@gmail.com>
2025-02-10 20:29:16 +0700

    complete

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,24 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1 , Rect R2){
+	double left,right,up,low,width,height;
+	left  = max(R1.x,R2.x);
+	right = min(R1.x+R1.w,R2.x+R2.w);
+	up = min(R1.y,R2.y);
+	low = max(R1.y-R1.h,R2.y-R2.h);
 
+	width = right-left;
+	height = up-low;
+	if(width <= 0 and height <= 0) return 0; 
+	else return width*height;
 
+}
 
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {2,2,5,5};	
+	cout << overlap(R1,R2);
+	
+	return 0;
 }
